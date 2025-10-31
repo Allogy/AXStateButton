62085332b625b564f4db8d3c94cfbb9a63988174
Jeremy Bone <jbone@allogy.com>
2025-10-31 09:06:38 -0500

    Add SPM support

--- AXStateButton.podspec
+++ AXStateButton.podspec
@@ -1,16 +1,16 @@
 Pod::Spec.new do |s|
   s.name            = "AXStateButton"
-  s.version         = "1.1.4"
+  s.version         = "1.2.0"
   s.license         = { :type  => 'MIT', :file => 'LICENSE.md' }
   s.summary         = "A simple UIButton subclass that allows for extensive button state customization."
   s.homepage        = "https://github.com/alexhillc/AXStateButton"
   s.author          = { "Alex Hill" => "alexhill.c@gmail.com" }
   s.source          = { :git => "https://github.com/alexhillc/AXStateButton.git", :tag => "v#{s.version}" }
 
-  s.platform        = :ios, '8.0'
+  s.platform        = :ios, '15.0'
   s.requires_arc    = true
 
-  s.source_files  = 'Source/*.{h,m}'
+  s.source_files  = 'Sources/AXStateButton/**/*.{h,m}'
   s.frameworks    = 'UIKit'
 end
 
---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AXStateButton",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "AXStateButton",
+            targets: ["AXStateButton"],
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "AXStateButton"
+        ),
+        .testTarget(
+            name: "AXStateButtonTests",
+            dependencies: ["AXStateButton"]
+        ),
+    ]
+)
